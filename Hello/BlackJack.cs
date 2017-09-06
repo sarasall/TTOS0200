74c4d103f34d011430513390d7ea1608b2816483
sarasall <K8500@student..jamk.fi>
2017-09-06 10:58:28 +0300

    Labrat project added for week tasks

--- Hello/BlackJack.cs
+++ Hello/BlackJack.cs
@@ -17,27 +17,28 @@ namespace Hello
         {
             int myNumber = 17;
             int theirNumber;
-            System.Console.WriteLine("*** BlackJack! ***");
-            System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
-            //reading and converting 
-            theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
-            //comparing that given umber is valid
-            if (theirNumber < 1 || theirNumber > 21)
+            
+            do
             {
-                Console.WriteLine("The given number is out of limits, try again.");
-            }
-            else
-            {
-                //comparing
+                System.Console.WriteLine("*** BlackJack! ***");
+                System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
+                //reading and converting 
+                theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
+                //comparing that given umber is valid
+
                 if (theirNumber >= myNumber && theirNumber <= 21)
                 {
                     System.Console.WriteLine("You win.");
                 }
-                else
+                if (theirNumber < 1 || theirNumber < 21)
                 {
                     System.Console.WriteLine("You lose.");
-                }
-            }
+                } 
+
+            } while (theirNumber != 0 || theirNumber < 21);
+
+ 
+
         }
     }
 }
---
+++ Labrat/Lab01.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Olio-ohjelmoinnin viikkotehtävät 
+//Sara Sallinen
+namespace Labrat
+{
+    class Lab01
+    {
+        public static void Exercise1()
+        {
+            int luku = 0;
+
+            Console.WriteLine("Anna luku: ");
+            luku = int.Parse(Console.ReadLine());
+
+            if (luku == 1)
+            {
+                Console.WriteLine("Annoit luvun yksi.");
+            }
+            if (luku == 2)
+            {
+                Console.WriteLine("Annoit luvun kaksi. ");
+            }
+            if (luku == 3)
+            {
+                Console.WriteLine("Annoit luvun kolme.");
+            }
+        }
+    }
+}
