94b78f841c0ead2ebbc383091111cd0ca779c8f4
Sara Sallinen <K8500@student.jamk.fi>
2017-10-13 15:45:28 +0300

    Added Labrat5.0: miniAsio & CD-classes

--- Labrat5.0/Properties/AssemblyInfo.cs
+++ Labrat5.0/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Labrat5")]
+[assembly: AssemblyTitle("Labrat5.0")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Labrat5")]
+[assembly: AssemblyProduct("Labrat5.0")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0582b88f-9bf3-4df0-870f-3e7f21ae32e3")]
+[assembly: Guid("c823801a-052e-4c32-8c5f-84adc0434c46")]
 
 // Version information for an assembly consists of the following four values:
 //
