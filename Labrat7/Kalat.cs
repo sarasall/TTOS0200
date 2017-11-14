e93f5b439701e34af5e6cf97a0f86482e0ee1640
Sara Sallinen <K8500@student.jamk.fi>
2017-11-14 23:13:00 +0200

    Added Lab8.1 & 8.2

--- Labrat7/Kalat.cs
+++ Labrat7/Kalat.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace JAMK.IT
 {
-    public class Kalat
+    public class Kala
     {
         public string Laji { get; set; }
         public double Pituus { get; set; }
@@ -14,7 +14,7 @@ namespace JAMK.IT
         public string PaikanNimi { get; set; }
         public string Paikka { get; set; }
 
-        public Kalat (string laji, double pituus, double paino, string nimi, string paikka)
+        public Kala (string laji, double pituus, double paino, string nimi, string paikka)
         {
             Laji = laji;
             Pituus = pituus;
@@ -27,17 +27,17 @@ namespace JAMK.IT
             return "Laji: " + Laji + " " + Pituus + " " + Paino + " kg." + "\nPaikka: " + PaikanNimi + " \nSijainti: " + Paikka;
         }
     }
-    public class Kalastajat
+    public class Kalastaja
     {
         public string Nimi { get; set; }
         public string PuhNum { get; set; }
-        public List<Kalat> kalalista { get; }
+        public List<Kala> kalalista { get; set; }
 
-        public Kalastajat (string nimi, string puhnum)
+        public Kalastaja (string nimi, string puhnum)
         {
             Nimi = nimi;
             PuhNum = puhnum;
-            kalalista = new List<Kalat>();
+            kalalista = new List<Kala>();
         }
         public override string ToString()
         {
@@ -51,20 +51,20 @@ namespace JAMK.IT
         {
             try
             {
-                Kalastajat kalastajaKirsi = new Kalastajat("Kirsi Kernel", "020-5555555");
+                Kalastaja kalastajaKirsi = new Kalastaja("Kirsi Kernel", "020-5555555");
                 Console.WriteLine("Uusi kalastaja lisätty rekisteriin: ");
                 Console.WriteLine(kalastajaKirsi.ToString());
 
                 Console.WriteLine("\nKalastaja: {0} sai uuden kalan", kalastajaKirsi.Nimi);
-                kalastajaKirsi.kalalista.Add(new Kalat("Ahven", 190, 5.0, "Jyväsjärvi", "Jyväskylä"));
+                kalastajaKirsi.kalalista.Add(new Kala("Ahven", 190, 5.0, "Jyväsjärvi", "Jyväskylä"));
                 Console.WriteLine(kalastajaKirsi.kalalista[0].ToString());
 
                 Console.WriteLine("\nKalastaja: {0} sai uuden kalan", kalastajaKirsi.Nimi);
-                kalastajaKirsi.kalalista.Add(new Kalat("Särki", 90, 2.5, "Oulujoki", "Oulu"));
+                kalastajaKirsi.kalalista.Add(new Kala("Särki", 90, 2.5, "Oulujoki", "Oulu"));
                 Console.WriteLine(kalastajaKirsi.kalalista[1].ToString());
 
                 Console.WriteLine("\nKaikki kalat rekisterissä:");
-                foreach (Kalat kala in kalastajaKirsi.kalalista)
+                foreach (Kala kala in kalastajaKirsi.kalalista)
                 {
                     Console.WriteLine(kala.ToString());
                 }
@@ -72,7 +72,7 @@ namespace JAMK.IT
                 kalastajaKirsi.kalalista.Sort((x, y) => y.Paino.CompareTo(x.Paino)); // Sortataan painavemmasta kevyempään kalaan
 
                 Console.WriteLine("\nJärjestetyt kalat rekisterissä:");
-                foreach (Kalat kala in kalastajaKirsi.kalalista)
+                foreach (Kala kala in kalastajaKirsi.kalalista)
                 {
                     Console.WriteLine(kala.ToString());
                 }
---
+++ Labrat8.2/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Labrat8._2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtSumma_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                double osto, myynti;
+                bool syote = double.TryParse(txtSumma.Text, out osto);
+
+                if (syote == true)
+                {
+                    if (combobValinta.SelectedItem == usValinta)
+                    {
+                        myynti = osto * 0.8997;
+                        txtbTulos.Text = myynti.ToString("0.00") + " euroa";
+                    }
+                    else if (combobValinta.SelectedItem == eurValinta)
+                    {
+                        myynti = osto / 0.8997;
+                        txtbTulos.Text = myynti.ToString("0.00") + " dollaria";
+                    }
+                }
+                else
+                {
+                    txtError.Text = "Virheellinen syöte!";
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
